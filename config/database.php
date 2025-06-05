b2f6977ba87b6deba1e8ff0d3c5c561350b48756
Nikolay4ru <90576767+Nikolay4ru@users.noreply.github.com>
2025-06-05 14:51:14 +0300

    test

--- config/database.php
+++ config/database.php
@@ -3,5 +3,5 @@
 $dbConfig = [
     'dsn' => 'mysql:host=localhost;dbname=koleso_app_task;charset=utf8mb4',
     'username' => 'root',
-    'password' => 'SecretQi159875321+A'
+    'password' => ''
 ];
--- config/mail.php
+++ config/mail.php
@@ -3,7 +3,7 @@ $mailConfig = [
     'smtp_host' => '192.168.0.8',
     'smtp_port' => 587,
     'smtp_username' => 'js@koleso-russia.ru',
-    'smtp_password' => '12345',
+    'smtp_password' => '',
     'from_email' => 'js@koleso-russia.ru',
     'from_name' => 'Task Management System'
 ];
