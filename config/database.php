56da4c5f160c8ab50e4ba9a896c34921cd84f78b
Nikolay4ru <90576767+Nikolay4ru@users.noreply.github.com>
2025-07-04 10:16:20 +0300

    fix

--- config/database.php
+++ config/database.php
@@ -2,5 +2,5 @@
 $dbConfig = [
     'dsn' => 'mysql:host=localhost;dbname=koleso_app_task;charset=utf8mb4',
     'username' => 'root',
-    'password' => 'SecretQi159875321+A'
+    'password' => ''
 ];
